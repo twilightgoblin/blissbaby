5bf70305a946e367667c15269833ce28b504bb0c
Twilight Goblin <ayushmanjunathas@gmail.com>
2026-01-09 13:32:09 +0530

    fix : update categories

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,15 +3,7 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-    seed: "npx tsx prisma/seed.ts",
-  },
   datasource: {
-    url: process.env["DATABASE_URL"] || "postgresql://localhost:5432/postgres",
-  },
-  generator: {
-    provider: "prisma-client-js",
+    url: process.env.DATABASE_URL!,
   },
 });
